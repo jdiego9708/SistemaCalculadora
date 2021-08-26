0b82db017de70ea22d2ca396718fdcdc03b29b77
JDiego9708 <jdiego9708@gmail.com>
2021-08-26 18:00:02 -0500

    Clase 4

--- SolutionSISCalculadora/SISCalculadora/Form1.Designer.cs
+++ SolutionSISCalculadora/SISCalculadora/Form1.Designer.cs
@@ -29,33 +29,137 @@ namespace SISCalculadora
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // txtNumero1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 56);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 59);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Mensaje";
-            this.button1.UseVisualStyleBackColor = true;
+            this.txtNumero1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNumero1.Location = new System.Drawing.Point(13, 68);
+            this.txtNumero1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(297, 45);
+            this.txtNumero1.TabIndex = 1;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNumero2.Location = new System.Drawing.Point(13, 155);
+            this.txtNumero2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(297, 45);
+            this.txtNumero2.TabIndex = 2;
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(318, 2);
+            this.btnSumar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(131, 56);
+            this.btnSumar.TabIndex = 3;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Location = new System.Drawing.Point(318, 68);
+            this.btnRestar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(131, 56);
+            this.btnRestar.TabIndex = 4;
+            this.btnRestar.Text = "Restar";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(318, 134);
+            this.btnDividir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(131, 56);
+            this.btnDividir.TabIndex = 5;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(318, 200);
+            this.btnMultiplicar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(131, 56);
+            this.btnMultiplicar.TabIndex = 6;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(13, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Número 2";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(479, 118);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(140, 32);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "Resultado ...";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(690, 274);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Formulario principal";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
 
--- SolutionSISCalculadora/SISCalculadora/Form1.cs
+++ SolutionSISCalculadora/SISCalculadora/Form1.cs
@@ -15,7 +15,80 @@ namespace SISCalculadora
         public Form1()
         {
             InitializeComponent();
+
+            this.btnSumar.Click += btnSumar_Click;
+            this.btnRestar.Click += BtnRestar_Click;
+            this.btnMultiplicar.Click += BtnMultiplicar_Click;
+            this.btnDividir.Click += BtnDividir_Click;
         }
 
+        private void BtnDividir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal numero1 = Convert.ToDecimal(this.txtNumero1.Text);
+                decimal numero2 = Convert.ToDecimal(this.txtNumero2.Text);
+
+                decimal resultado = numero1 / numero2;
+
+                this.lblResultado.Text = $"Resultado = {resultado}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error = {ex.Message}");
+            }
+        }
+
+        private void BtnMultiplicar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero1 = Convert.ToInt32(this.txtNumero1.Text);
+                int numero2 = Convert.ToInt32(this.txtNumero2.Text);
+
+                int resultado = numero1 * numero2;
+
+                this.lblResultado.Text = $"Resultado = {resultado}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error = {ex.Message}");
+            }
+        }
+
+        private void BtnRestar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero1 = Convert.ToInt32(this.txtNumero1.Text);
+                int numero2 = Convert.ToInt32(this.txtNumero2.Text);
+
+                int resultado = numero1 - numero2;
+
+                this.lblResultado.Text = $"Resultado = {resultado}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error = {ex.Message}");
+            }
+        }
+
+        //Este botón es para sumar dos números
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero1 = Convert.ToInt32(this.txtNumero1.Text);
+                int numero2 = Convert.ToInt32(this.txtNumero2.Text);
+
+                int resultado = numero1 + numero2;
+
+                this.lblResultado.Text = $"Resultado = {resultado}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error = {ex.Message}");
+            }                     
+        }
     }
 }
